bff93cfce42c6d3c4151f65a56ad78f1958577ae
serrinaqqad <140744938+serrinaqqad@users.noreply.github.com>
2023-08-08 22:11:08 +0300

    Update Admin.java

--- src/main/java/Sakancom/util/Admin.java
+++ src/main/java/Sakancom/util/Admin.java
@@ -14,9 +14,15 @@ public class Admin {
 
     public Admin() {
         username ="eman42kh@gmail.com";
-        password="123456";
+        password="654321";
         owners = new ArrayList<>();
+        owners.add(new Owner("1", "serrin test", "serrin@example.com", "1234567890", "Location1"));
+        owners.add(new Owner("2", "lara test", "lara@example.com", "0987654321", "Location2"));
     }
+    public void reset() {
+        logState = false;
+    }
+
 
     public Admin (String username ,String password,String email,String phone) {
         this.username=username;
@@ -35,17 +41,22 @@ public class Admin {
     }
 
     public boolean login(String username,String password) {
+    	 System.out.println("Provided username: " + username);
+    	    System.out.println("Provided password: " + password);
         if(logState) {
+            System.out.println("Admin is already logged in.");
             return false;
         } else {
             if(username.equals("eman42kh@gmail.com") && password.equals("654321")) {
                 logState=true;
                 return true;
             } else {
+                System.out.println("Incorrect username or password provided.");
                 return false;
             }
         }
     }
+
 
     public void logOut() {
         if(logState) {
