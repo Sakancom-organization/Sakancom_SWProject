a6750fae18cecc22a2a0fb66201b65e04065a118
serrinaqqad <140744938+serrinaqqad@users.noreply.github.com>
2023-08-08 22:11:35 +0300

    Update AdminLogsInSteps.java

--- src/test/java/Sakancom/AdminLogsInSteps.java
+++ src/test/java/Sakancom/AdminLogsInSteps.java
@@ -1,5 +1,6 @@
 package Sakancom;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -9,6 +10,13 @@ import Sakancom.util.Admin;
 
 public class AdminLogsInSteps {
   Admin admin = AdminCommonSteps.getAdmin();
+  @Before
+  public void setUp() {
+      admin = AdminCommonSteps.getAdmin();
+      if (admin != null) {
+          admin.reset();
+      }
+  }
 
   @Given("I am an admin")
   public void i_am_an_admin() {
@@ -17,8 +25,10 @@ public class AdminLogsInSteps {
 
   @When("I enter my correct username and password")
   public void i_enter_my_correct_username_and_password() {
-    assertTrue(admin.login("eman42kh@gmail.com", "654321"));
+      System.out.println("Admin logState before login: " + admin.getLoginState());
+      assertTrue(admin.login("eman42kh@gmail.com", "654321"));
   }
+
 
   @Then("I should be logged in")
   public void i_should_be_logged_in() {
